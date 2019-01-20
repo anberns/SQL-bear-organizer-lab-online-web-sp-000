ba8bdba35ad3ecbcd73d68d7d58cf522a4cd0f72
Aaron Berns <anberns@gmail.com>
2019-01-20 15:44:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX(prom_color) FROM bears GROUP BY COUNT(color) as prom_color"
+  "SELECT MAX(color_count) FROM bears WHERE "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -29,3 +29,14 @@ end
 def selects_bear_that_killed_Tim
   "Write your SQL query here"
 end
+
+
+SELECT customerid,
+       firstname,
+       lastname
+  FROM customers
+ WHERE supportrepid IN (
+           SELECT employeeid
+             FROM employees
+            WHERE country = 'Canada'
+       );
